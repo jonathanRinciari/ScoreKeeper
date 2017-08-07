883b83553b7ec4009e81957fe3920767dcb27dbb
Jonathan <jrinciari@or-specialties.com>
2017-08-06 20:51:34 -0400

    refactored and changed logic

--- scorekeeper.js
+++ scorekeeper.js
@@ -4,7 +4,7 @@ var playerTwo = document.querySelector("#playerTwo");
 var p1Score = document.querySelector("span");
 var p2Score = document.querySelector("#p2score");
 var scoreTracker = document.querySelector("#scoreTracker");
-var input = document.querySelector("input");
+var numInput = document.querySelector("input[type='number']");
 var score1 = 0;
 var score2 = 0;
 var scoreTracking = 0;
@@ -15,7 +15,7 @@ var winningScore = 5;
 playerOne.addEventListener("click", function(){
   if (!gameOver){
 	score1 ++;
-
+	console.log(score1, winningScore);
 	if(score1 === winningScore){
 		gameOver = true;
 		p1Score.classList.add("winner");
@@ -23,21 +23,31 @@ playerOne.addEventListener("click", function(){
 	p1Score.textContent = score1;
 }
 
-  })
+  });
 
 playerTwo.addEventListener("click", function(){
 	if (!gameOver){
 	   score2 ++;
-
 		if (score2 === winningScore){
 			gameOver = true;
 			p2Score.classList.add("winner");	
 		}
 		p2Score.textContent = score2;
 	}
-})
+});
 
 reset.addEventListener("click", function(){
+	scoreReset();
+});
+
+numInput.addEventListener("change", function(){
+	
+	scoreTracker.textContent = this.value;
+	winningScore = Number(this.value);
+	scoreReset();
+});
+
+function scoreReset(){
 	score1 = 0
 	score2 = 0
 	p1Score.textContent = score1;
@@ -46,13 +56,7 @@ reset.addEventListener("click", function(){
 	p2Score.classList.remove("winner");
 	gameOver = false;
 
-
-})
-
-input.addEventListener("click", function(){
-	scoreTracking++;
-	scoreTracker.textContent = scoreTracking;
-})
+}
 
 
 
